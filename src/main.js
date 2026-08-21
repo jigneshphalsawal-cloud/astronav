52879cde882da4d94198020869d8e5243b61dc16
jigneshphalsawal-cloud <jigneshphalsawal@gmail.com>
2026-08-21 16:22:41 +0530

    add deploy workflow

--- src/main.js
+++ src/main.js
@@ -5,19 +5,20 @@ document.querySelector("#app").innerHTML = "<p>loading...</p>";
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
 .then(response => response.json()).then(data => {
-    document.querySelector("#app").innerHTML = `
-        <h1>${data.title}</h1>
-        <img src="${data.url}" />
-        <p>${data.explanation}</p>
-    `;
-})
-let media;
+    let media;
 
 if (data.media_type === "image") {
     media = `<img src="${data.url}"/>`;
 } else {
     media = `<video src="${data.url}" controls></video>`;
 }
+
+    document.querySelector("#app").innerHTML = `
+        <h1>${data.title}</h1>
+        <img src="${data.url}" />
+        <p>${data.explanation}</p>
+    `;
+})
 
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
